075b6ed29bc5ed6b2914ced3502643e728777aaf
Dre Peeters <103488303+drepeeters@users.noreply.github.com>
2022-12-07 13:37:24 +0100

    Update check_members_removal.py

--- check_members_removal.py
+++ check_members_removal.py
@@ -2,6 +2,14 @@ import pathlib
 
 members_dir = pathlib.Path("./contents")
 
+members_list = []
+for members in members_dir.glob('*.md'):
+  print(members)
+  members_list.append(members)
+  
+print(members_list)
+ 
+  
 f = open("day1.txt", "r")
 
 print(f.read())
