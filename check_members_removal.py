c6d1c945035f5bfa3d35b37fae705e42a49382bc
Dre Peeters <103488303+drepeeters@users.noreply.github.com>
2022-12-07 13:19:05 +0100

    Update check_members_removal.py

--- check_members_removal.py
+++ check_members_removal.py
@@ -4,5 +4,5 @@ members_dir = pathlib.Path("./contents")
 
 with open('members.txt', 'w') as fp:
     for member in members_dir.glob('*.md'):
-        fp.write(member)
+        fp.write(str(member))
         fp.write('\n')
