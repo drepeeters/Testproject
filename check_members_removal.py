9bbfeeba9e6a927de21fae984212460c46a4fa49
Dre Peeters <103488303+drepeeters@users.noreply.github.com>
2022-12-07 13:56:45 +0100

    Update check_members_removal.py

--- check_members_removal.py
+++ check_members_removal.py
@@ -8,7 +8,7 @@ for members in members_dir.glob('*.md'):
   members_list.append(str(members).split('/')[1])
   
 print(members_list)
-print(members_dir.home())
+print(members_dir.absolute())
 
 #with open('members_cache.txt', 'w') as f:
  #   for member in members_list:
