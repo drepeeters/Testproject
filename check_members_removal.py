869ed88b5a9f0b7bba7c98e41bfa3eb9bb986778
Dre Peeters <103488303+drepeeters@users.noreply.github.com>
2022-12-07 15:37:37 +0100

    Update check_members_removal.py

--- check_members_removal.py
+++ check_members_removal.py
@@ -5,20 +5,16 @@ members_dir = pathlib.Path("./contents")
 
 current_members = []
 for members in members_dir.glob('*.md'):
-  print(str(members).split('/')[1])
   current_members.append(str(members).split('/')[1])
   print("Loaded all the members currently in contents")
   
-
+  
 previous_members = []
 with open("members_cache.txt", 'r') as f:
     for line in f:
         previous_members.append(line.rstrip('\n'))
     print("Previous list of members is loaded")
  
-
-print(current_members)
-print(previous_members)
 
 if set(previous_members).issubset(set(current_members)):
     print('Same List')
